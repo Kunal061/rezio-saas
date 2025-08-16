397d82a707b4a6ccd4b5128701928edf432d2f43
Aayush <aayush130405@gmail.com>
2025-08-16 13:51:56 +0530

    add social-share page

--- app/(app)/social-share/page.tsx
+++ app/(app)/social-share/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client"
+import React, { useState , useEffect, useRef } from 'react'
+import { CldImage } from 'next-cloudinary';
 
-function SocialShare() {
+const socialFormats = {
+  "Instagram square (1:1)": {width: 1080, height: 1080, aspectRatio: "1:1"},
+  "Instagram Portrait (4:5)": {width: 1080, height: 1350, aspectRatio: "4:5"},
+  "Twitter Post (16:9)": {width: 1200, height: 675, aspectRatio: "16:9"},
+  "Twitter Header (3:1)": {width: 1500, height: 500, aspectRatio: "3:1"},
+  "Facebook Cover (205:78)": {width: 820, height: 312, aspectRatio: "205:78"},
+};
+
+type SocialFormat = keyof typeof socialFormats;
+
+export default function SocialShare() {
+
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null)
+  const [selectedFormat, setSelectedFormat] = useState<SocialFormat>("Instagram square (1:1)")
+  const [isUploading, setIsUploading] = useState(false)
+  const [isTransforming, setIsTransforming] = useState(false)
+  const imageRef = useRef<HTMLImageElement>(null)
+
+  useEffect(() => {
+    if(uploadedImage) {
+      setIsTransforming(true)
+    }
+  }, [selectedFormat, uploadedImage])
+
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if(!file) return
+    setIsUploading(true)
+    const formData = new FormData()
+    formData.append("file", file)
+
+    try {
+      const response = await fetch("api/image-upload", {
+        method: "POST",
+        body: formData
+      })
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div>SocialShare</div>
   )
 }
-
-export default SocialShare
